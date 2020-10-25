13e78c7db9703b4aa967e2a159d336a10682af6b
mkoterski <matthiaskoterski@gmail.com>
2020-10-25 18:24:01 +0100

    2020-10-25 Update 3

--- testbed/python/relay-poc01.py
+++ testbed/python/relay-poc01.py
@@ -1,46 +1,51 @@
 #!/usr/bin/env python
-# Simple pump test script with menu
+# Simple relay test script with menu
 # 
-# Last Update: 2020-10-06
+# Last Update: 2020-10-25
 # Modified by: Matthias Koterski
 #
 # Change log: 
 # 
-# 0.1a - Initial version. LED turns on and off every second.
+# 0.1a - Initial WIP version.
 #
-# Pump is connected to pin XX, relais is connected to pin XX
+# Relay is connected to pin 7 (GPIO4)
+#
+# Requirements:
+# 1. Raspberry Pi B+ model
+# 2. Relay controller
+# 3. Water pump
+#
+# Working Instructions:
+# Step 1: Connect any voltage pin from Raspberry Pi to Relay.
+# Step 2:Connect any ground pin from Raspberry pi to Relay.
+# Step 3: Connect any GPIO pin to Relay module (In this project I used pin 7(GPIO4)).
+# Step 4: Open terminal and execute the program we provided below.
 #
 ################################################################################################################
 
-
-# Deactivate error warnings
-# GPIO.setwarnings(False)
-
-
 # To discover information about your RPi:
-
 # GPIO.RPI_INFO
 
 # To discover the Raspberry Pi board revision:
-
 # GPIO.RPI_INFO['P1_REVISION']
 
 # To discover the version of RPi.GPIO:
-
 # GPIO.VERSION
-
-
-
-
 
 import RPi.GPIO as GPIO
 import time
 
+
 # import only system from os 
 from os import system, name 
 
-# Set #27 as LED pin
-RelayPin = 27
+
+#GPIO.setwarnings(False) # Deactivate warning messages
+GPIO.setmode(GPIO.BOARD) # Set pin layout to BOARD instead of BCM
+GPIO.setup(7,GPIO.OUT)   # Set pin to No. 7 (GPIO4)
+
+# Set #7 (GPIO4) as relay pin
+RelayPin = 7
 
 # define our clear function 
 def clear_screen(): 
@@ -58,11 +63,11 @@ def menu():
     clear_screen()
     print (50 * '-')
     print ("")
-    print ("           P U M P   T E S T   0.1 \n")
-    print ("Ensure that the pump is connected to XXXXXXXX\n")
+    print ("           R E L A Y   T E S T   0.1 \n")
+    print ("Ensure that the relay is connected to GPIO4 (pin #7)\n")
     print (50 * '-')
-    print ("1 - PUMP ON")
-    print ("2 - PUMP OFF")
+    print ("1 - Relay ON")
+    print ("2 - Relaf OFF")
     print ("3 - Close program")
     print (50 * '-')
      
@@ -71,15 +76,17 @@ def menu():
      
     # Take action as per selected menu-option ###
     if choice == 1:
-            print ("Pump turning on...")
+            print ("Relay turning on...")
             setup()
-            GPIO.output(RelayPin, GPIO.LOW)
+            GPIO.output(7,True)
+            #GPIO.output(RelayPin, GPIO.LOW)
             time.sleep(3)
             menu()
     elif choice == 2:
-            print ("Pump turning off...")
+            print ("Relay turning off...")
             setup()
-            GPIO.output(RelayPin, GPIO.HIGH)
+            GPIO.output(7,False)
+            #GPIO.output(RelayPin, GPIO.HIGH)
             time.sleep(3)
             menu()
     elif choice == 3:
@@ -94,16 +101,16 @@ def menu():
 # Define a setup function for some setup
 def setup():
     # Set the GPIO modes to BOARD numbering so that the code can be easily reused
-    GPIO.setmode(GPIO.BCM)
+#    GPIO.setmode(GPIO.BCM)
     # Set RelayPin's mode to output, 
     # and initial level to High(3.3v)
-    GPIO.setup(RelayPin, GPIO.OUT, initial=GPIO.HIGH)
+    #GPIO.setup(RelayPin, GPIO.OUT, initial=GPIO.HIGH)
 
 # Define a destroy function for clean up everything afterwards
 
 def destroy():
-    # Turn off LED
-    GPIO.output(RelayPin, GPIO.HIGH)
+    # Turn off Relay
+#    GPIO.output(RelayPin, GPIO.HIGH)
     # Release resource
     GPIO.cleanup()                     
 
