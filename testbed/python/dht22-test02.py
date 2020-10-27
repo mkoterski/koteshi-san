765fb09e9347a168e8d94cc8a2f07a6fef9e5f46
mkoterski <matthiaskoterski@gmail.com>
2020-10-27 08:51:21 +0100

    Create dht22-test02.py

---
+++ testbed/python/dht22-test02.py
@@ -0,0 +1,11 @@
+#Libraries
+import Adafruit_DHT as dht
+from time import sleep
+#Set DATA pin
+DHT = 4
+while True:
+    #Read Temp and Hum from DHT22
+    h,t = dht.read_retry(dht.DHT22, DHT)
+    #Print Temperature and Humidity on Shell window
+    print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(t,h))
+    sleep(5) #Wait 5 seconds and read again
