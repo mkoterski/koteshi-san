964af50b729a851545fd3e581e79de3090850a91
mkoterski <matthiaskoterski@gmail.com>
2020-11-07 16:14:57 +0100

    2020-11-07 Update 1

---
+++ testbed/python/soil-poc02.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# Simple relay test script with menu
+# 
+# Last Update: 2020-11-07
+# Modified by: Matthias Koterski
+#
+# Change log: 
+# 0.1a - Initial WIP version.
+#
+# Requirements:
+# 1. Raspberry Pi B+ model
+# 2. Relay controller
+# 3. Water pump
