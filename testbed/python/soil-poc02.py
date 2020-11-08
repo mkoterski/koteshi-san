4dd0fd7958346ee8a9a0ea78c5141166b0edc84e
mkoterski <matthiaskoterski@gmail.com>
2020-11-08 13:46:15 +0100

    Update soil-poc02.py

--- testbed/python/soil-poc02.py
+++ testbed/python/soil-poc02.py
@@ -1,13 +1,31 @@
-#!/usr/bin/env python
-# Simple relay test script with menu
+#!/usr/bin/python
+#
+# Simple soil moisture sensor test v2
 # 
-# Last Update: 2020-11-07
+# Last Update: 2020-11-08
+#
 # Modified by: Matthias Koterski
 #
-# Change log: 
-# 0.1a - Initial WIP version.
+# Modified original script found at http://www.piddlerintheroot.com/soil-moisture-sensor/ and converted it to Python 3.
 #
-# Requirements:
-# 1. Raspberry Pi B+ model
-# 2. Relay controller
-# 3. Water pump
+
+import RPi.GPIO as GPIO
+import time
+ 
+#GPIO SETUP
+channel = 21
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(channel, GPIO.IN)
+ 
+def callback(channel):
+        if GPIO.input(channel):
+                print("Water Detected!")
+        else:
+                print("Water Detected!")
+ 
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
+GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
+ 
+# infinite loop
+while True:
+        time.sleep(1)
