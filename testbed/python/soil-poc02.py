c46cf638836c5efc881411f8f6b2485264eaa02b
mkoterski <matthiaskoterski@gmail.com>
2020-11-08 13:59:32 +0100

    Update soil-poc02.py

--- testbed/python/soil-poc02.py
+++ testbed/python/soil-poc02.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python
-#
 # Simple soil moisture sensor test v2
 # 
 # Last Update: 2020-11-08
-#
 # Modified by: Matthias Koterski
+#
+# Change log: 
+# 
+# 0.1a - Initial WIP version.
+#
+# To do:
+# Convert to BOARD layout
+# Implement destroy routine
+# Add clear screen
+#
+# Connect sensor to Pin 21 and 3.3V
 #
 # Modified original script found at http://www.piddlerintheroot.com/soil-moisture-sensor/ and converted it to Python 3.
 #
@@ -13,7 +22,7 @@ import RPi.GPIO as GPIO
 import time
  
 #GPIO SETUP
-channel = 21
+channel = 40
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(channel, GPIO.IN)
  
@@ -25,7 +34,14 @@ def callback(channel):
  
 GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
 GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
- 
+
+# Define a destroy function for clean up everything afterwards
+# def destroy():
+#     # Turn off Relay
+#     GPIO.output(7,False)
+#     # Release resource
+#     GPIO.cleanup()     
+
 # infinite loop
 while True:
         time.sleep(1)
