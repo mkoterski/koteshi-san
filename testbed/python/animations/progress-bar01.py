ad1ef14050fa34597c76eacbb4a18170d84c96e6
mkoterski <matthiaskoterski@gmail.com>
2020-11-09 17:32:54 +0100

    Update progress-bar01.py

--- testbed/python/animations/progress-bar01.py
+++ testbed/python/animations/progress-bar01.py
@@ -24,16 +24,17 @@ def progress(count, total, status=''):
     bar_len = 60
     filled_len = int(round(bar_len * count / float(total)))
 
-    percents = round(100.0 * count / float(total), 1)
+    percents = round(100.1 * count / float(total), 1)
     bar = '=' * filled_len + '-' * (bar_len - filled_len)
 
-    sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
+    sys.stdout.write('\r[%s] %s%s ...%s\r' % (bar, percents, '%', status))
+    # sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
     sys.stdout.flush()  # As suggested by Rom Ruben (see: http://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console/27871113#comment50529068_27871113)
 
 
-total = 1000
+total = 100
 i = 0
 while i < total:
-    progress(i, total, status='Doing very long job')
+    progress(i, total, status='Doing random job')
     time.sleep(0.5)  # emulating long-playing job
     i += 1
