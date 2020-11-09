8f272cd70631a437debda35201c6f1eef43440b9
mkoterski <matthiaskoterski@gmail.com>
2020-11-09 17:52:49 +0100

    Create simple-bar01.py

---
+++ testbed/python/animations/simple-bar01.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+#
+
+import time
+
+bar = [
+    " [=     ]",
+    " [ =    ]",
+    " [  =   ]",
+    " [   =  ]",
+    " [    = ]",
+    " [     =]",
+    " [    = ]",
+    " [   =  ]",
+    " [  =   ]",
+    " [ =    ]",
+]
+i = 0
+
+while True:
+    print(bar[i % len(bar)], end="\r")
+    time.sleep(.2)
+    i += 1
