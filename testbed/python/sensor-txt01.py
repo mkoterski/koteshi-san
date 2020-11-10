b007ac91440874fdaa965b6b4df7d7b8df18a0e2
mkoterski <matthiaskoterski@gmail.com>
2020-11-10 22:00:10 +0100

    Update sensor-txt01.py
    Added more documentation and tidied up the code

--- testbed/python/sensor-txt01.py
+++ testbed/python/sensor-txt01.py
@@ -1,19 +1,33 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 #
-# When writing a file, it follows the sequence of open, write, close
+# Simple test to write DHT sensor data to .txt file
+# If log file does not exists yet, it will be created
+# 
+# Last Update: 2020-11-10
+# Modified by: Matthias Koterski
 #
+# Change log: 
+# 
+# 0.1a - Initial version.
 #
+# Requirements:
+# 1. Raspberry Pi B+ model
+# 2. DHT sensor
+# 3. Jumper cables
+# 4. CircuitPython-DHT library (run "pip3 install adafruit-circuitpython-dht && sudo apt-get install libgpiod2")
+# (5. Depending on the sensor model you may need a pull up resistor)
+#
+####################################################################################################################
 
 import time
 import os.path
 import board
 import adafruit_dht
 
-#comment and uncomment the lines below depending on your sensor
+#comment and uncomment the lines below depending on your sensor. Here the sensor is connected to pin #7 / GPIO4
 #sensor = Adafruit_DHT.DHT11
 sensor = adafruit_dht.DHT22(board.D4, use_pulseio=False)
 #sensor = Adafruit_DHT.AM2302
-
 
 #create a variable to control the while loop
 running = True
@@ -34,16 +48,16 @@ while running:
         humidity = sensor.humidity
         temperature = sensor.temperature
 
-		#humidity, temperature = Adafruit_DHT.read_retry(sensor, sensor_pin)
-
-        #print temperature and humidity
+        #print temperature and humidity to screen
         print("Temperature: " + str(temperature) + " °C, " + "Humidity: " + str(humidity) + " %")
+        
         #save date, time, temperature in Celsius, and humidity in sensor_log01.txt file
         log_file.write(time.strftime("%Y-%m-%d,%H:%M:%S") + "," + str(temperature) + "," + str(humidity) + "\n")
-        time.sleep(1)
+        time.sleep(1.0)
 
     except RuntimeError as error:
     # In case you are interested in the error message you can uncomment the following line
+    # Believe me, they are usually not that interesting
     #    print(error.args[0]) 
         time.sleep(2.0)
         continue
@@ -57,12 +71,4 @@ while running:
         running = False
         log_file.close()
 
-time.sleep(3.0)
-
-
-
-
-
-
-
-
+time.sleep(2.0)
