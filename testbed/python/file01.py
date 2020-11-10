230d41a4408660c95acf4bceea54cb53ee78c8d6
mkoterski <matthiaskoterski@gmail.com>
2020-11-10 14:58:19 +0100

    Update file01.py

--- testbed/python/file01.py
+++ testbed/python/file01.py
@@ -1,4 +1,15 @@
-file01.py
 #!/usr/bin/python
+#
+# When writing to files, it follows the sequence of open, write, close
+#
+#
 
 import time
+
+text_to_write = "Sample text to save!\n" # \n for line break
+
+save_file = open("example_file.txt","w") # "r" read, "w" write, or "a" append
+
+save_file.write(text_to_write)
+
+save_file.close()
