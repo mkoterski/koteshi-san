b82574211d6a4f9dcaba6b97ab998d6fc674aaef
mkoterski <matthiaskoterski@gmail.com>
2020-11-11 21:37:22 +0100

    Update bme280-test01.py

--- testbed/python/bme280-test01.py
+++ testbed/python/bme280-test01.py
@@ -8,6 +8,9 @@
 import smbus2
 import bme280
 
+#only required for dew point calculation
+import math
+
 port = 1
 address = 0x76
 bus = smbus2.SMBus(port)
@@ -18,12 +21,19 @@ calibration_params = bme280.load_calibration_params(bus, address)
 # compensated_reading object
 data = bme280.sample(bus, address, calibration_params)
 
+# dew point calculation
+b = 17.62
+c = 243.12
+gamma = (b * data.temperature /(c + data.temperature)) + math.log(data.humidity / 100.0)
+dew_point = (c * gamma) / (b - gamma)
+
 # the compensated_reading class has the following attributes
 print(data.id)
 print(data.timestamp)
 print(data.temperature)
 print(data.pressure)
 print(data.humidity)
+print(dew_point)
 
 # there is a handy string representation too
 print(data)
