cb8955e607c02febb32621699fd9a4bf249f89cf
mkoterski <matthiaskoterski@gmail.com>
2020-11-14 14:54:05 +0100

    Update mcp3008-test01.py

--- testbed/python/mcp3008-test01.py
+++ testbed/python/mcp3008-test01.py
@@ -1,37 +1,30 @@
-
 #! /usr/bin/env python
 # python programa to comunicate with an MCP3008
 # Import our SpiDev wrapper and our sleep function
+
 import spidev
 from time import sleep
 
 # Establish SPI device on Bus 0,Device 0
 spi = spidev.SpiDev()
 spi.open(0,0)
-spi.max_speed_hz=1000000
 
 def getAdc (channel):
-              #check valid channel
-              if ((channel>7)OR(channel<0)):
-                       return -1
+    #check valid channel
+    if ((channel>7)or(channel<0)):
+        return -1
 
-              # Preform SPI transaction and store returned bits in 'r'
-              r = spi.xfer([1, (8+channel) << 4, 0])
+    # Preform SPI transaction and store returned bits in 'r'
+    r = spi.xfer([1, (8+channel) << 4, 0])
 
-              #Filter data bits from retruned bits
-              adcOut = ((r[1]&3) << 8) + r[2]
-              
-              if ((adcOut > 778)OR(adcOut < 386)):
-                      print("Out of range error", adcOut)
-                       
-              else:
-                       percent = ((778 - adcOut) / 392)*100
+    #Filter data bits from retruned bits
+    adcOut = ((r[1]&3) << 8) + r[2]
+    percent = int(round(adcOut/10.24))
 
-                       #print out 0-1023 value and percentage
-                       print("ADC Output: {:3} Percentage: {:.1f} %".format (adc,percent ))
-                          
+    #print out 0-1023 value and percentage
+    print(("ADC Output: {0:4d} Percentage: {1:3}%".format (adcOut,percent)))
+    sleep(0.1)
 
 while True:
-              getAdc(0)
-              sleep (1)
-              
+    getAdc(0)
+
